9c0aff2aa0b231510ef8cf1a5fa5d223a9bc5db7
Leonard Apanasevich <apana@fnal.gov>
2015-06-12 16:42:02 -0500

    Switch to AK4 jets

--- python/l1RecoTreeProducer_cfi.py
+++ python/l1RecoTreeProducer_cfi.py
@@ -2,8 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 l1RecoTreeProducer = cms.EDAnalyzer("L1RecoTreeProducer",
 
-  jetTag                  = cms.untracked.InputTag("ak5CaloJets"),
-  jetIdTag                = cms.untracked.InputTag("ak5JetID"),
+  jetTag                  = cms.untracked.InputTag("ak4CaloJets"),
+  jetIdTag                = cms.untracked.InputTag("ak4JetID"),
   metTag                  = cms.untracked.InputTag("met"),
   ebRecHitsTag            = cms.untracked.InputTag("ecalRecHit:EcalRecHitsEB"),
   eeRecHitsTag            = cms.untracked.InputTag("ecalRecHit:EcalRecHitsEE"),
@@ -13,7 +13,7 @@ l1RecoTreeProducer = cms.EDAnalyzer("L1RecoTreeProducer",
   basicClustersEndcapTag  = cms.untracked.InputTag("multi5x5BasicClusters:multi5x5EndcapBasicClusters"),
   verticesTag             = cms.untracked.InputTag("offlinePrimaryVertices"),
   tracksTag               = cms.untracked.InputTag("generalTracks"),
-  jetCorrectorServiceName = cms.untracked.string("ak5CaloL1FastL2L3"),
+  jetCorrectorServiceName = cms.untracked.string("ak4CaloL1FastL2L3"),
   #jetCorrectorServiceName = cms.untracked.string("ak5CaloL1L2L3Residual"),
 
   maxCl  = cms.uint32(20),
